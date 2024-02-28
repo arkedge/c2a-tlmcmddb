538bbce36261c31b71df8e540db6ae7a43cef01d
KOBAYASHI Kazuhiro <kobayashi-kazuhiro@arkedgespace.com>
2024-02-28 10:22:17 +0900

    csv: Include line number in error message

--- tlmcmddb-csv/src/lib.rs
+++ tlmcmddb-csv/src/lib.rs
@@ -21,6 +21,54 @@ where
     }
 }
 
+#[derive(Debug)]
+struct PositionError<E> {
+    position: csv::Position,
+    error: E,
+}
+
+impl<E> std::fmt::Display for PositionError<E>
+where
+    E: std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Error at line {}:  {}",
+            self.position.line() + 1,
+            self.error
+        )
+    }
+}
+
+impl<E> std::error::Error for PositionError<E>
+where
+    E: std::error::Error + 'static,
+{
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.error)
+    }
+}
+
+trait ErrWithPosition {
+    type T;
+    type E;
+    fn err_with_position(self, position: &csv::Position) -> Result<Self::T, PositionError<Self::E>>
+    where
+        Self: Sized;
+}
+
+impl<T, E> ErrWithPosition for Result<T, E> {
+    type T = T;
+    type E = E;
+    fn err_with_position(self, position: &csv::Position) -> Result<T, PositionError<E>> {
+        self.map_err(|error| PositionError {
+            position: position.clone(),
+            error,
+        })
+    }
+}
+
 impl<'r, R: 'r> Iterator for PosStringRecordIterator<R>
 where
     R: std::io::Read,
